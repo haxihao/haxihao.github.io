ed6126f4a1fff6b047db660e1084070bd566418e
哈希豪 <1214110654@qq.com>
2024-10-08 15:40:01 +0800

    1

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -4,7 +4,6 @@ import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue'
 </script>
 <template>
     <YunFooter>
-        <!-- 自定义页脚内容 -->
         <VCLiveTime mt="2" start-time="2024-08-31">
             <template #live-time-before>
                 <span>本站已运行</span>
